b2c11de07e144d0c3187d27581ad88d37ea0a762
Devesh Saini <57091455+deveshsainii@users.noreply.github.com>
2024-06-10 23:34:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router"
 import Navbar from "./component/navbar"
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
+import { Qm } from './constants';  // Import Qm
 
 function App() {
 
